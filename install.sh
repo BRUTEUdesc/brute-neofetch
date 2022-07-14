08f1b3ea77c4b74bda0c4ece5af4f95acc58ca7d
Joao Oliveira <joaomarcosnet10@gmail.com>
2022-07-14 11:33:34 -0300

    update install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ path=$(pwd)
 cd /tmp
 git clone https://github.com/BRUTEUdesc/brute-neofetch
 mv $HOME/.config/neofetch $HOME/.config/neofetch.bkp
-cp -r brute-neofetch/.config/neofetch/ $HOME/.config/neofetch
+cp -rT brute-neofetch/home/ $HOME/
 cd $path
 rm -rf /tmp/brute-neofetch
