b480c913e7f3a59b24337333d95b962ea626acc6
Joao Oliveira <joaomarcosnet10@gmail.com>
2022-07-14 17:40:21 -0300

    update installer

--- install.sh
+++ install.sh
@@ -9,26 +9,25 @@ red_color="\e[0;31m"
 white_color="\e[0m"
 green_flag="\e[0m[\e[0;32m+\e[0m]"
 red_flag="\e[0m[\e[0;31m-\e[0m]"
-white_flag="[*]"
+white_flag="\e[0m[*]"
 
 set -- $(locale LC_MESSAGES)
 yesexpr="$1"; noexpr="$2"; yesword="$3"; noword="$4"
 curren_path=$(pwd) 
 
 echo -e "$green_flag Iniciando instalação do neofetch do Brute!\n"
-sleep 0.5
+sleep 1
 
 if [[ -d "$HOME/.config/neofetch" ]]
 then
     if [[ -d "$HOME/.config/neofetch.bkp" ]]
     then
-        echo -e "$green_flag Já existe um backup do neofetch.\n"
-        sleep 0.5
+        echo -e "$white_flag Já existe um backup do neofetch.\n"
+        sleep 1
     else
         mv $HOME/.config/neofetch $HOME/.config/neofetch.bkp
-        mkdir $HOME/.config/neofetch
         echo -e "$green_flag Backup da configuração atual do neofetch feito.\n"
-        sleep 0.5
+        sleep 1
     fi
 else
     mkdir $HOME/.config/neofetch
@@ -41,39 +40,32 @@ echo -e "- Logo do Fedora: $blue_color$white_color"
 echo -e "- Logo do Mac: $pink_color$white_color"
 
 while true; do
-    read -p "[*] ${yesword}/${noword}: " yn
+    read -p "[*] (${yesword}/${noword})? " yn
     if [[ "$yn" =~ $yesexpr ]]; then nf=true; break; fi
     if [[ "$yn" =~ $noexpr ]]; then nf=false; break; fi
-    echo -e "Answer ${yesword}/${noword}."
+    echo "Answer ${yesword}/${noword}."
 done
 
-sleep 0.5
-
-if [[ $nf ]]
+if [[ $nf == true ]]
 then
     if [[ $OSTYPE =~ 'linux' ]] 
-    then 
-        #install for linux with nerd fonts
-        echo "install for linux with nerd fonts"
-        sleep 0.5
+    then
+        echo "linux install with nerd fonts"
     elif [[ $OSTYPE =~ 'darwin' ]] 
     then
-        #install for macos with nerd fonts
-        sleep 0.5
-        echo "install for macos with nerd fonts"
+        echo "macos install with nerd fonts"
+    else
+        echo "$green_flag Não consegui detectar seu sistema operacional :("
     fi
 else
-    #install without nerd fonts
     if [[ $OSTYPE =~ 'linux' ]] 
-    then 
-        #install for linux without nerd fonts
-        echo "install for linux without nerd fonts"
-        sleep 0.5
+    then
+        echo "linux install without nerd fonts"
     elif [[ $OSTYPE =~ 'darwin' ]] 
     then
-        #install for macos without nerd fonts
-        echo "install for macos without nerd fonts"
-        sleep 0.5
+        echo "macos install without nerd fonts"
+    else
+        echo "$green_flag Não consegui detectar seu sistema operacional :("
     fi
 fi
 
