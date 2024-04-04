e61be99ce5ccdd3797daa346a1fcc758b3b6b7d9
João Oliveira <joaomarcosnet10@gmail.com>
2024-04-04 00:57:13 -0300

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,20 @@ set -- $(locale LC_MESSAGES)
 yesexpr="$1"; noexpr="$2"; yesword="$3"; noword="$4"
 curren_path=$(pwd) 
 
-ok="[OK]"
-ast="[*]"
-error="[X]"
+WHITE='\033[0;37m'
+GREEN='\033[0;32m'
+LIGHT_GRAY='\033[0;37m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+ORANGE='\033[0;38;5;208m'
+BLUE='\033[0;34m'
+PINK='\033[0;95m'
+NC='\033[0m'
+
+ok="${WHITE}[${GREEN}OK${WHITE}]${NC}"
+ast="${WHITE}[${LIGHT_GRAY}*${WHITE}]${NC}"
+warn="${WHITE}[${YELLOW}*${WHITE}]${NC}"
+error="${WHITE}[${RED}X${WHITE}]${NC}"
 
 if [[ $(command -v neofetch) =~ 'neofetch' ]]
 then
@@ -26,4 +37,7 @@ then
     mv $HOME/.config/neofetch.bkp $HOME/.config/neofetch
 fi
 
-neofetch
+if [[ $(command -v neofetch) =~ 'neofetch' ]]
+then
+    neofetch
+fi
